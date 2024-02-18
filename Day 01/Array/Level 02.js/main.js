d5b27fce290310c1521edb48d2a96f9943c54466
Awanthi Malawanage <121420903+Awanthiy@users.noreply.github.com>
2024-02-18 13:20:14 +0530

    Create main.js

---
+++ Day 01/Array/Level 02.js/main.js
@@ -0,0 +1,5 @@
+import { countries } from "./countries.js";
+import { webTechs } from "./web_techs.js";
+
+console.log("First few countries:", countries.slice(0, 5));
+console.log("Web technologies:", webTechs);
